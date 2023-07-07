b193d348caf00f33a7c088c01f998f40de72ccd0
kangjun091120 <kangjun1120@outlook.com>
2023-07-07 21:24:01 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,75 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <el-container>
+    <el-header>
+      <el-button type="primary">登录</el-button>
+    </el-header>
+    <el-main>
+      <div class="products-grid">
+        <div class="product-card" v-for="product in products" :key="product.id">
+          <el-card :body-style="{ padding: '0px' }">
+            <div style="padding: 14px;">
+              <div class="product-name">{{ product.name }}</div>
+              <div class="product-price">{{ product.price }}</div>
+            </div>
+            <el-image
+              fit="cover"
+              :src="product.image"
+              class="product-image"
+            />
+            <div style="padding: 14px;">
+              <el-button type="primary">购买</el-button>
+            </div>
+          </el-card>
+        </div>
+      </div>
+    </el-main>
+  </el-container>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        { id: 1, name: '商品1', price: '￥100', image: 'image1.jpg' },
+        { id: 2, name: '商品2', price: '￥200', image: 'image2.jpg' },
+        { id: 3, name: '商品3', price: '￥300', image: 'image3.jpg' },
+        // 添加更多商品...
+      ]
+    }
+  }
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.el-header {
+  display: flex;
+  justify-content: flex-end;
+  padding: 20px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.products-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  gap: 20px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.product-card {
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1);
+}
+
+.product-name {
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.product-price {
+  color: #ff4500;
+}
+
+.product-image {
+  height: 200px;
+  object-fit: cover;
 }
 </style>
